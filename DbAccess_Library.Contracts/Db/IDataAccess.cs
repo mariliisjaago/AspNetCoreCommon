12ee9a01d6fa06d137eabba252b65b7d9e9e9b76
Mariliis Jaago <mariliisjaago@hotmail.com>
2021-02-27 12:56:04 +0200

    Created functionality to query from and save to database (as generics).

---
+++ DbAccess_Library.Contracts/Db/IDataAccess.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DbAccess_Library.Contracts.Db
+{
+    public interface IDataAccess
+    {
+        Task<List<T>> LoadData<T, U>(string sqlStatement, U parameters, string connectionStringName);
+        Task<int> SaveData<U>(string sqlStatement, U parameters, string connectionStringName);
+    }
+}
