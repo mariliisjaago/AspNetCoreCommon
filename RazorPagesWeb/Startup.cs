c4ef86017b46c096abe1b4999957e2e626d34d11
Mariliis Jaago <mariliisjaago@hotmail.com>
2021-02-27 19:18:10 +0200

    RazorPages web app is made and then configured with dependency injection.

---
+++ RazorPagesWeb/Startup.cs
@@ -0,0 +1,65 @@
+using DbAccess_Library.Contracts.Db;
+using DbAccess_Library.Contracts.Repos;
+using DbAccess_Library.Db;
+using DbAccess_Library.Repos;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace RazorPagesWeb
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+
+            services.AddSingleton(new ConnectionStringData
+            {
+                SqlConnectionName = "Default"
+            });
+
+            services.AddSingleton<IDataAccess, DataAccess>();
+
+            services.AddScoped<IFoodsRepo, SqlFoodsRepo>();
+            services.AddScoped<IOrdersRepo, SqlOrdersRepo>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapRazorPages();
+            });
+        }
+    }
+}
