759f7fd6be5429d72fb853a7431c6002d6a75f2b
Mariliis Jaago <mariliisjaago@hotmail.com>
2021-02-27 10:51:29 +0200

    Add project files.

---
+++ TimsDinerDb/Scripts/Script.PostDeployment1.sql
@@ -0,0 +1,19 @@
+﻿/*
+Post-Deployment Script Template							
+--------------------------------------------------------------------------------------
+ This file contains SQL statements that will be appended to the build script.		
+ Use SQLCMD syntax to include a file in the post-deployment script.			
+ Example:      :r .\myfile.sql								
+ Use SQLCMD syntax to reference a variable in the post-deployment script.		
+ Example:      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+
+if not exists (select * from dbo.Foods)
+begin
+    insert into dbo.Foods (Title, [Description], Price)
+    values ('Cheeseburger Meal', 'A cheeseburger, fries, and a drink', 5.95),
+    ('Chilli dog Meal', 'A chilli dog, fries, and a drink', 4.15),
+    ('Vegan Meal', 'A large salad and a water', 1.95);
+end
---
+++ TimsDinerDb/dbo/Tables/Orders.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Orders]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [OrderName] NVARCHAR(50) NOT NULL, 
+    [OrderDate] DATETIME2 NOT NULL, 
+    [FoodId] INT NOT NULL, 
+    [Quantity] INT NOT NULL, 
+    [Total] MONEY NOT NULL, 
+    CONSTRAINT [FK_Orders_Foods] FOREIGN KEY (FoodId) REFERENCES Foods(Id)
+)
