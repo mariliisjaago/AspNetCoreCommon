1676d61b643278592044de7e8456d25e3e71b97a
Mariliis Jaago <mariliisjaago@hotmail.com>
2021-03-05 20:33:31 +0200

    In MVC UI: added functionality to display  a person's order, and display the menu.

---
+++ MVCWeb/Controllers/FoodController.cs
@@ -0,0 +1,23 @@
+﻿using DbAccess_Library.Contracts.Repos;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace MVCWeb.Controllers
+{
+    public class FoodController : Controller
+    {
+        private readonly IFoodsRepo _foodsRepo;
+
+        public FoodController(IFoodsRepo foodsRepo)
+        {
+            _foodsRepo = foodsRepo;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var foods = await _foodsRepo.GetAllFoods();
+
+            return View(foods);
+        }
+    }
+}
--- MVCWeb/Controllers/OrdersController.cs
+++ MVCWeb/Controllers/OrdersController.cs
@@ -12,11 +12,13 @@ namespace MVCWeb.Controllers
     {
         private readonly IFoodsRepo _foodsRepo;
         private readonly IPlaceOrderStrategy _placeOrderStrategy;
+        private readonly IGetOrderStrategy _getOrderStrategy;
 
-        public OrdersController(IFoodsRepo foodsRepo, IPlaceOrderStrategy placeOrderStrategy)
+        public OrdersController(IFoodsRepo foodsRepo, IPlaceOrderStrategy placeOrderStrategy, IGetOrderStrategy getOrderStrategy)
         {
             _foodsRepo = foodsRepo;
             _placeOrderStrategy = placeOrderStrategy;
+            _getOrderStrategy = getOrderStrategy;
         }
 
         public IActionResult Index()
@@ -56,7 +58,20 @@ namespace MVCWeb.Controllers
 
             int id = await _placeOrderStrategy.Place(viewModel.Order);
 
-            return RedirectToAction("Create");
+            return RedirectToAction("Display", new { id });
+        }
+
+        public async Task<IActionResult> Display(int id)
+        {
+            OrderDisplayModel orderDisplay = new OrderDisplayModel();
+
+            (OrderModel order, string foodTitle) data = await _getOrderStrategy.GetOrderAndFoodTitle(id);
+
+            orderDisplay.Order = data.order;
+
+            orderDisplay.ItemPurchased = data.foodTitle;
+
+            return View(orderDisplay);
         }
     }
 }
