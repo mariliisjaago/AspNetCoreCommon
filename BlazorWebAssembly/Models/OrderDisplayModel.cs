b35e0c37355b560ce5573077c3781cabd1ad53e1
Mariliis Jaago <mariliisjaago@hotmail.com>
2021-03-27 20:19:40 +0200

    Added functionality to display a order with details. Added functionality to display the food menu.

---
+++ BlazorWebAssembly/Models/OrderDisplayModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorWebAssembly.Models
+{
+    public class OrderDisplayModel
+    {
+        public OrderModel Order { get; set; }
+        public string ItemPurchased { get; set; }
+
+    }
+}
